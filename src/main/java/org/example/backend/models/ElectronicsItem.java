c84290d96c28c9ce3c51f860d18b3bca5afd1354
Vinh Phan <neilvinh.phan@gmail.com>
2026-03-27 22:40:31 +0700

    fix: fix cách đặt tên file

--- src/main/java/org/example/backend/models/ElectronicsItem.java
+++ src/main/java/org/example/backend/models/ElectronicsItem.java
@@ -2,19 +2,17 @@ package org.example.backend.models;
 
 import java.time.LocalDateTime;
 
-public class Vehicle_Item extends Item {
+public class ElectronicsItem extends Item {
   protected String brand;
-  protected String model;
-  protected int manufacturingYear; // Năm sản xuất
-  protected double mileage; // số km đã đi
+  protected int warrantyMonths; // Số tháng bảo hành
+  protected String condition; // tình trạng của sản phẩm
 
-  public Vehicle_Item(
+  public ElectronicsItem(
       int id,
       String itemName,
       String brand,
-      String model,
-      int manufacturingYear,
-      double mileage,
+      int warrantyMonths,
+      String condition,
       String description,
       LocalDateTime startTime,
       LocalDateTime endTime,
@@ -22,34 +20,29 @@ public class Vehicle_Item extends Item {
       double currentHightestBid) {
     super(id, itemName, description, startTime, endTime, startingPrice, currentHightestBid);
     this.brand = brand;
-    this.model = model;
-    this.manufacturingYear = manufacturingYear;
-    this.mileage = mileage;
+    this.warrantyMonths = warrantyMonths;
+    this.condition = condition;
   }
 
-  public double getMileage() {
-    return mileage;
-  }
-
-  public int getManufacturingYear() {
-    return manufacturingYear;
-  }
-
-  public String getModel() {
-    return model;
+  public String getCondition() {
+    return condition;
   }
 
   public String getBrand() {
     return brand;
   }
 
+  public int getWarrantyMonths() {
+    return warrantyMonths;
+  }
+
   public void printInfo() {
     System.out.println("Mã sản phẩm" + id);
     System.out.println("Xe " + getItemName());
     System.out.println("Thương hiệu: " + brand);
-    System.out.println("Năm sản xuất:" + manufacturingYear);
-    System.out.println("Số km đã đi: " + mileage);
-    System.out.println("Tổng quan về chiếc xe: " + description);
+    System.out.println("Số tháng bảo hành sản phẩm " + warrantyMonths);
+    System.out.println("Tình trạng sản phẩm: " + condition);
+    System.out.println("Mô tả sản phẩm: " + description);
     System.out.println("Thời gian bắt đầu đấu giá: " + startTime);
     System.out.println("Thời gian đóng phiên đấu giá: " + endTime);
     System.out.println("Giá khởi điểm: " + getStartingPrice());
--- src/main/java/org/example/backend/services/AuthService.java
+++ src/main/java/org/example/backend/services/AuthService.java
@@ -1,4 +1,3 @@
 package org.example.backend.services;
 
-public class Login {
-}
+public class AuthService {}
