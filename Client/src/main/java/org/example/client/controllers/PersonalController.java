73aaad51e8fef7506ce38f26d52e5ad111e9508d
25023329-crypto <25023329@vnu.edu.vn>
2026-04-22 14:24:47 +0700

    feat: CreateAuction ui

--- Client/src/main/java/org/example/client/controllers/PersonalController.java
+++ Client/src/main/java/org/example/client/controllers/PersonalController.java
@@ -1,17 +1,12 @@
 package org.example.client.controllers;
 
-import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
-import javafx.stage.Stage;
 
-public class PersonalController extends BaseController{
+public class PersonalController extends BaseController {
     @FXML
     private MenuButton menuDanhMuc;
     @FXML
@@ -19,29 +14,41 @@ public class PersonalController extends BaseController{
     @FXML
     private MenuButton menuUser;
     @FXML
+    private Button createAuction;
+
+    @FXML
     private void handleMenuItem(ActionEvent event) {
         MenuItem item = (MenuItem) event.getSource();
         MenuButton parentMenu = (MenuButton) item.getParentPopup().getOwnerNode();
         parentMenu.setText(item.getText());
-        switchScene(event,"/views/AuctionCatalogView.fxml","Danh mục sản phẩm đấu giá");
-
+        switchScene(event, "/views/AuctionCatalogView.fxml", "Danh mục sản phẩm đấu giá");
     }
+
     @FXML
-    private void handleRoomAuction(ActionEvent event){
+    private void handleRoomAuction(ActionEvent event) {
         MenuItem item = (MenuItem) event.getSource();
         MenuButton parentMenu = (MenuButton) item.getParentPopup().getOwnerNode();
         parentMenu.setText(item.getText());
     }
+
+    @FXML
+    void handleCreateAuction(ActionEvent event) {
+        switchScene(event, "/views/CreateAuctionView.fxml", "Tạo cuộc đấu giá");
+    }
+
     @FXML
     void handleMain(ActionEvent event) {
         switchScene(event, "/views/MainView.fxml", "Trang chủ");
     }
+
     @FXML
-    void handleLogout(ActionEvent event){
+    void handleLogout(ActionEvent event) {
         switchScene(event, "/views/LoginView.fxml", "Đăng nhập hệ thống");
     }
+
     @FXML
-    void handleUserui(ActionEvent event) {switchScene(event, "/views/PersonalView.fxml", "Hồ sơ cá nhân");
+    void handleUserui(ActionEvent event) {
+        switchScene(event, "/views/PersonalView.fxml", "Hồ sơ cá nhân");
     }
 
 }
