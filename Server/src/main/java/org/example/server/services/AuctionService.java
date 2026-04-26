11f60452596a1e0c95f2dffecf69c9ff5f33750e
Vinh Phan <neilvinh.phan@gmail.com>
2026-04-26 23:41:39 +0700

    feat: update AuctionS

--- Server/src/main/java/org/example/server/services/AuctionService.java
+++ Server/src/main/java/org/example/server/services/AuctionService.java
@@ -18,7 +18,7 @@ public class AuctionService {
   private static final ItemDAO itemDAO = ItemDAO.getInstance();
 
   // ==========================================
-  // 📦 1. NHÓM KHỞI TẠO (CHUẨN BỊ LÊN SÀN)
+  //  1. NHÓM KHỞI TẠO (CHUẨN BỊ LÊN SÀN)
   // ==========================================
 
   public static Auction createAuction(AuctionRequestDTO requestPayLoad) throws Exception {
@@ -37,7 +37,7 @@ public class AuctionService {
       throw new Exception("Vật phẩm đang được đấu giá!");
     }
 
-    // Khởi tạo Auction mới (Nó sẽ tự nhận trạng thái WAREHOUSE từ Constructor của bro)
+    // Khởi tạo Auction mới (Nó sẽ tự nhận trạng thái WAREHOUSE từ Constructor)
     Auction newAuction = new Auction(checkItem, durationMinutes);
 
     // TODO 3: Gọi AuctionDAO.insert(newAuction) để lưu nháp xuống DB.
@@ -48,46 +48,55 @@ public class AuctionService {
   // Lấy các phiên đấu giá theo trạng thái
   public static List<Auction> getAuctionsByStatus(AuctionStatus status) throws Exception {
     // TODO: Gọi DAO lấy danh sách các phòng đấu giá theo trạng thái (Ví dụ: Lấy các phòng RUNNING
-    List<Auction> auction = AuctionDAO.getInstance().getAllAuctionByStatus(status);
+    List<Auction> auction = AuctionDAO.getInstance().getAllAuctionsByStatus(status);
     return auction;
   }
 
   // ==========================================
-  // 🚀 2. NHÓM VẬN HÀNH (ĐIỀU KHIỂN LUỒNG)
+  // 2. NHÓM VẬN HÀNH (ĐIỀU KHIỂN LUỒNG)
   // ==========================================
 
   public static void openAuction(int auctionId) throws Exception {
-    // 1. Lôi phòng đấu giá từ DB lên
-    // Auction auction = auctionDAO.getAuctionById(auctionId);
-    // if (auction == null) throw new Exception("Không tìm thấy phiên đấu giá!");
+    // Lấy phiên đấu giá từ DB lên
+    Auction auction = auctionDAO.getAuctionByAuctionId(auctionId);
+    if (auction == null) throw new Exception("Không tìm thấy phiên đấu giá!");
 
-    // 2. Ra lệnh cho Entity tự chạy logic của nó
-    // auction.start(LocalDateTime.now());
+    // Bắt đầu phiên đấu giá (Auction.java)
+    auction.start(LocalDateTime.now());
 
-    // 3. Lưu trạng thái mới (RUNNING) và startTime, endTime xuống Database
-    // auctionDAO.updateAuction(auction);
+    // Lưu trạng thái mới (RUNNING) và startTime, endTime xuống Database
+    auctionDAO.setAuctionStatus(auctionId, AuctionStatus.RUNNING);
   }
 
   public static void forceCancelAuction(int auctionId, String reason) throws Exception {
     // TODO: Lấy Auction lên, set trạng thái thành CANCELED và update xuống DB.
     // (Dành cho Admin hoặc người bán hủy ngang khi có biến)
+    Auction auction = auctionDAO.getAuctionByAuctionId(auctionId);
+    AuctionStatus status = auction.getStatus();
+    if (status == AuctionStatus.CANCELED) throw new Exception("Phiên ấu giá đã bị hủy!");
+
+    auctionDAO.setAuctionStatus(auctionId, AuctionStatus.CANCELED);
+
+    // Còn reason chưa biết lưu đâu
   }
 
   // ==========================================
-  // 🔄 3. NHÓM HỖ TRỢ ĐẤU GIÁ (CHO BIDDING SERVICE GỌI)
+  // 3. NHÓM HỖ TRỢ ĐẤU GIÁ (CHO BIDDING SERVICE GỌI)
   // ==========================================
 
   public static Auction getAuctionById(int auctionId) throws Exception {
-    // TODO: Lấy thông tin phòng đấu giá. BiddingService sẽ dùng hàm này để check liên tục.
-    return null;
+    // Lấy thông tin phòng đấu giá. BiddingService sẽ dùng hàm này để check liên tục.
+    Auction auction = auctionDAO.getAuctionByAuctionId(auctionId);
+    return auction;
   }
 
   public static void updateHighestBid(int auctionId, BidTransaction newBid) throws Exception {
-    // TODO: Gọi DAO cập nhật ID của người đang trả giá cao nhất vào bảng Auction.
+    // Gọi DAO cập nhật ID của người đang trả giá cao nhất vào bảng Auction.
+
   }
 
   // ==========================================
-  // ⏱️ 4. NHÓM TỰ ĐỘNG ĐÓNG PHÒNG (AUTO-CLOSE)
+  // 4. NHÓM TỰ ĐỘNG ĐÓNG PHÒNG (AUTO-CLOSE)
   // ==========================================
 
   // Dùng Lazy Check hay Background Job (Luồng ngầm) để xử lý các phòng hết giờ?
