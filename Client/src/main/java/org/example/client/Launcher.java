7e060dab0d8f16fa37e42b175a14de61e65c18d9
Vinh Phan <neilvinh.phan@gmail.com>
2026-05-18 12:27:06 +0700

    feat: run with launcher

---
+++ Client/src/main/java/org/example/client/Launcher.java
@@ -0,0 +1,8 @@
+package org.example.client;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Gọi gián tiếp sang hàm main của JavaFX để lách luật
+        ClientMain.main(args);
+    }
+}
